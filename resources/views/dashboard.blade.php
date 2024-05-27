9fdab45660b3bff65827ac6e4e5b84c1a910890c
Nathanael <nathanael6556@gmail.com>
2024-05-27 15:19:57 +0700

    Fix cropped text for no movie in a cinema

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,16 +21,16 @@
 </head>
 
 
-<body>   
+<body>
     <header>
         <div id="menu-icon" class='bx bx-menu'></div>
         <ul class="navbar">
             <li><a href="#home" class="home-active">HOME</a></li>
             <li><a href="#movies">MOVIES</a></li>
             <li><a href="#coming">COMING</a></li>
-            <li><a href="#newsletter">NEWSLETTER</a></li>            
-            <li><a href="{{ route('user.profile') }}" id="profile" >PROFILE</a></li>
-            <li><a href="{{ route('logout') }}" id="logout" >LOGOUT</a></li>
+            <li><a href="#newsletter">NEWSLETTER</a></li>
+            <li><a href="{{ route('user.profile') }}" id="profile">PROFILE</a></li>
+            <li><a href="{{ route('logout') }}" id="logout">LOGOUT</a></li>
         </ul>
     </header>
     <section class="home swiper" id="home">
@@ -64,30 +64,30 @@
                         <a href="/detail" class="btn">TRAILER</a>
                     </div>
                 </div>
-            </div>            
+            </div>
         </div>
         <div class="swiper-pagination"></div>
     </section>
 
 
     <div class="flex-container">
-        <div class="wrapper">    
+        <div class="wrapper">
             <div class="dropdown">
                 <label for="cityDropdown">City&emsp;</label>
                 <select id="cityDropdown" onchange="getCinema()">
-                   
+
                 </select>
             </div>
         </div>
 
 
-        <div class="wrapper">    
+        <div class="wrapper">
             <div class="dropdown">
-                <label for="cinemaDropdown" >Cinema&ensp;</label>
+                <label for="cinemaDropdown">Cinema&ensp;</label>
                 <select id="cinemaDropdown" onchange="getFilm()">
                 </select>
             </div>
-        </div>    
+        </div>
     </div>
 
 
@@ -105,36 +105,36 @@
             <div class="box">
                 <div class="box-img">
                     <img src="{{ asset('storage/assets/film5_potrait.jpg') }}">
-                </div>                    
+                </div>
             </div>
             <div class="box">
                 <div class="box-img">
                     <img src="{{ asset('storage/assets/film4_potrait.jpg') }}">
-                </div>  
+                </div>
             </div>
             <div class="box">
                 <div class="box-img">
                     <img src="{{ asset('storage/assets/film3_potrait.jpg') }}">
-                </div>                    
+                </div>
             </div>
             <div class="box">
                 <div class="box-img">
                     <img src="{{ asset('storage/assets/film2_potrait.jpg') }}">
-                </div>                    
+                </div>
             </div>
             <div class="box">
                 <div class="box-img">
                     <img src="{{ asset('storage/assets/film1_potrait.jpg') }}">
-                </div>                    
+                </div>
             </div>
         </div>
     </section>
     <script>
-        document.addEventListener("DOMContentLoaded", function() {
+        document.addEventListener("DOMContentLoaded", function () {
             var movieDescElements = document.querySelectorAll('.movie-desc');
 
 
-            movieDescElements.forEach(function(descElement) {
+            movieDescElements.forEach(function (descElement) {
                 var maxLength = 100; // Set the maximum length to 10 characters
                 var originalText = descElement.textContent.trim();
 
@@ -155,7 +155,8 @@
             <div class="col">
                 <h4>FOLLOW US ON</h4>
                 <div class="social">
-                    <a href="https://www.instagram.com/alicia.claresta31?igsh=MXR4MmRibGYwbXhleg==" target="_blank" class="social-icons"><i class='bx bxl-instagram'></i></a>
+                    <a href="https://www.instagram.com/alicia.claresta31?igsh=MXR4MmRibGYwbXhleg==" target="_blank"
+                        class="social-icons"><i class='bx bxl-instagram'></i></a>
                     <a href="#" class="social-icons"><i class='bx bxl-twitter'></i></a>
                     <a href="#" class="social-icons"><i class='bx bxl-youtube'></i></a>
                 </div>
@@ -165,7 +166,7 @@
 </body>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script>
-    $(document).ready(function(){
+    $(document).ready(function () {
         loadCity();
         console.log(success);
         if (success == "Success") {
@@ -173,54 +174,54 @@
             window.location.href = "http://127.0.0.1:8000/user/dashboard#home";
             // console.log($("#back").click());
         };
-   
-});
 
-
-function loadCity(){
-    $.ajax({
-        url: 'get-cities',
-        type: 'get',
-        success: function (response) {
-            console.log(response);
-            $.each(response.cities, function(index, city) {
-                $('#cityDropdown').append($('<option>', {
-                    value: city.id,  
-                    text: city.name  
-                }));
-            });
-            getCinema();
-        },
-        error: function (error) {
-            console.log(error);
-        }
     });
-};
-// <option hidden disabled selected value="">--Silahkan Pilih Waktu--</option>
 
 
-function getCinema(){
-            $('#movies-container').html("");
-            $("#cinemaDropdown").find("option").remove();
-            $('#cinemaDropdown').append($('<option>', {
-                        value: '',  
-                        text: "-- Select Cinema --",
-                        selected: true,
-                        disabled: true,
-                        hidden: true  
-                    }));
-            var id = $("#cityDropdown").val();
-            console.log(id);
-            $.ajax({
-            url: 'get-cinemas/'+id,
+    function loadCity() {
+        $.ajax({
+            url: 'get-cities',
             type: 'get',
             success: function (response) {
                 console.log(response);
-           
-                $.each(response.cinemas, function(index, cinema) {
+                $.each(response.cities, function (index, city) {
+                    $('#cityDropdown').append($('<option>', {
+                        value: city.id,
+                        text: city.name
+                    }));
+                });
+                getCinema();
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    };
+    // <option hidden disabled selected value="">--Silahkan Pilih Waktu--</option>
+
+
+    function getCinema() {
+        $('#movies-container').html("");
+        $("#cinemaDropdown").find("option").remove();
+        $('#cinemaDropdown').append($('<option>', {
+            value: '',
+            text: "-- Select Cinema --",
+            selected: true,
+            disabled: true,
+            hidden: true
+        }));
+        var id = $("#cityDropdown").val();
+        console.log(id);
+        $.ajax({
+            url: 'get-cinemas/' + id,
+            type: 'get',
+            success: function (response) {
+                console.log(response);
+
+                $.each(response.cinemas, function (index, cinema) {
                     $('#cinemaDropdown').append($('<option>', {
-                        value: cinema.id,  
-                        text: cinema.name  
+                        value: cinema.id,
+                        text: cinema.name
                     }));
                 });
             },
@@ -229,33 +230,29 @@ function getCinema(){
             }
         });
     };
-    function getFilm(){
-            $('#movies-container').html("");
-            var cinemaId = $("#cinemaDropdown").val();
-            console.log(cinemaId);
-            $.ajax({
-            url: 'get-films-lengkap/'+cinemaId,
+    function getFilm() {
+        $('#movies-container').html("");
+        var cinemaId = $("#cinemaDropdown").val();
+        console.log(cinemaId);
+        $.ajax({
+            url: 'get-films-lengkap/' + cinemaId,
             type: 'get',
-            dataType: 'json',          
+            dataType: 'json',
             success: function (response) {
                 console.log(response);
-                if(!response.success){
+                if (!response.success) {
                     $("#movies-container").append(`
-                        <div class='box' data-aos='fade-up' data-aos-duration='2500'>
+                        <div class='box-none' data-aos='fade-up' data-aos-duration='2500'>
                             <h3>There is no movie playing in this cinema from today until ${response.until}</h3>
                         </div>
                     `)
                 }
 
-
-
-
-                $.each(response.films, function(index, film){
+                $.each(response.films, function (index, film) {
                     $('#movies-container').append(
                         "<div class='box' data-aos='fade-up' data-aos-duration='2500'>" +
                         "<div class='box-img'>" +
                         `<img src = "{{ asset('storage/assets/${film[0].poster_potrait}')}}" alt= "${film[0].judul}" >`+
-                        // "<img src='" + "{{ asset('storage/assets/"+ film[0].judul +"') }}" + "' alt='" + film[0].judul + "'>" +
                         "<form action='{{ route('user.detail') }}' method='post'>" +
                         "<input type='hidden' name='_token' value='{{ csrf_token() }}' autocomplete='off'>" +
                         "<div class='overlay'>" +
@@ -266,154 +263,16 @@ function getCinema(){
                         "</div>" +
                         "</div>" +
                         "<h3>" + film[0].judul + "</h3>" +
-                        "<span>" + film[0].name + " | " + film[0].age_cat + "</span>" +                        
+                        "<span>" + film[0].name + " | " + film[0].age_cat + "</span>" +
                         "</form>" +
                         "</div>");
-
-
-                })  
-
-
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
-    }
-</script>
-</html>
-
-
-{{-- @section('style')
-<style>
-    h1 {
-        color: red;
-    }
-</style>
-@endsection
-
-
-@section('body')
-<select name="city" id="city" onchange="getCinema()">
-    <option value="-1" selected> </option>
-</select>
-<select name="cinema" id="cinema" onchange="getFilm()">
-    <option value="-1" selected> </option>
-</select>
-<p>Daftar Film</p>
-<div id="filmsHere"></div>
-
-
-@endsection
-
-
-@section('script')
-<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
-
-
-<script>
-    Swal.fire({
-        title: 'Berhasil!',
-        text: 'Memasangkan script sweet alert',
-        icon: 'success',
-        confirmButtonText: 'Cool'
-    })
-    $(document).ready(function(){
-        loadCity();
-       
-    });
-    function loadCity(){
-        $.ajax({
-            url: 'get-cities',
-            type: 'get',
-            success: function (response) {
-                console.log(response);
-                $.each(response.cities, function(index, city) {
-                    $('#city').append($('<option>', {
-                        value: city.id,  
-                        text: city.name  
-                    }));
-                });
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
-    };
-    function getCinema(){
-            $("#cinema").find("option").remove();
-            $('#cinema').append($('<option>', {
-                        value: '-1',  
-                        text: " ",
-                        selected: true  
-                    }));
-            var id = $("#city").val();
-            console.log(id);
-            $.ajax({
-            url: 'get-cinemas/'+id,
-            type: 'get',
-            success: function (response) {
-                console.log(response);
-                $.each(response.cinemas, function(index, cinema) {
-                    $('#cinema').append($('<option>', {
-                        value: cinema.id,  
-                        text: cinema.name  
-                    }));
-                });
-            },
-            error: function (error) {
-                console.log(error);
-            }
-        });
-    };
-    function getFilm(){
-            $('#filmsHere').html("");
-            var cinemaId = $("#cinema").val();
-            console.log(cinemaId);
-            $.ajax({
-            url: 'get-films/'+cinemaId,
-            type: 'get',
-            success: function (response) {
-                console.log(response);
-                $.each(response.films, function(index, film){
-                    $('#filmsHere').append(
-                        "<form action='{{ route('user.get-detail') }}' method='post'> " +
-                        "<input type='hidden' name='_token' value='{{ csrf_token() }}' autocomplete='off'>"+
-                        "<p> " + film[0].judul + "<br>"+ film[0].description +
-                        "<input type='hidden' name='film_id' value='"+film[0].id+"'>"+
-                        "<input type='hidden' name='cinema_id' value='"+cinemaId+"'>"+
-                         "<br><button class='btn btn-primary'>Buy Ticket</button>"
-                         + "</p>"+ "</form>");
                 })
-                /*
-                for (let i = 0; i < response.data1.films.length; i++) {
-                    var film = response.data1.films[i];
-                    var studio = response.data2.studios[i];
-                    var st = response.showtime.showtimes[i];
-                    // "<form action='"++"'' method='post'>" +
-                        // "<input type='hidden' name='"+st+"'>"
-                        // +
-                        // </form>
-                    $('#filmsHere').append(
-                        "<p> " + film[0].judul + "<br>" + studio[0].name + "<br>" + film[0].description +
-                       
-                         "<br><button class='btn btn-primary' id='"+st+"' onClick='redirectFilm' >Buy Ticket</button>"
-                        + "</p>");
-                }
-                 
-                $orders = Order::where('user_id', 1)
-    ->where('status', 'completed')
-    ->where('another_column', 'some_value') // Add additional conditions as needed
-    ->select('columns')
-    ->get();
-                */
             },
             error: function (error) {
                 console.log(error);
             }
         });
     }
-
-
 </script>
-@endsection --}}
+
+</html>
